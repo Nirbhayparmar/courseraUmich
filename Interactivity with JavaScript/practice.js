32b11ed2160fb81d6ef6f2696c59112d638260e9
NirbhayParmar <niravnirbhay94gmail.com>
2021-01-08 18:16:41 +0530

    completed js

---
+++ Interactivity with JavaScript/practice.js
@@ -0,0 +1,25 @@
+function colorchange() {
+  var id = prompt("enter");
+  var x = document.getElementById(id);
+  //console.log(x);
+  x.className = "color";
+}
+
+function colorchange1() {
+  var id = prompt("enter");
+  var x = document.getElementById(id);
+  //console.log(x);
+  x.className = "color1";
+}
+function hiden() {
+  var id = prompt("enter");
+  var x = document.getElementById(id);
+  //console.log(x);
+  x.className = "hide";
+}
+function visibled() {
+  var id = prompt("enter");
+  var x = document.getElementById(id);
+  //console.log(x);
+  x.className = "visible";
+}
